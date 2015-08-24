0368e1c0f37299fdda73536b72419e2172602c38
bcdxn <bcdixon@outlook.com>
2015-08-24 19:10:40 -0400

    Add 'release' task to gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -7,10 +7,13 @@ var gulp             = require('gulp'),
     less             = require('gulp-less'),
     CleanCssPlugin   = require('less-plugin-clean-css'),
     AutoprefixPlugin = require('less-plugin-autoprefix'),
+    Path             = require('path'),
     cleancss         = new CleanCssPlugin({ advanced: true }),
     autoprefix       = new AutoprefixPlugin({ browsers: ['last 2 versions'] }),
     rename           = require('gulp-rename'),
     watch            = require('gulp-watch'),
+    clean            = require('gulp-clean'),
+    runSequence      = require('run-sequence'),
     path             = {};
 
 path.LESS         = './src/**/*.less';
@@ -28,7 +31,7 @@ path.OUT_JS_MIN   = 'bundle.min.js';
 
 // LESS
 gulp.task('less', function () {
-  gulp.src(path.LESS_ENTRY)
+  return gulp.src(path.LESS_ENTRY)
     .pipe(less({
       plugins: [autoprefix, cleancss]
     }))
@@ -38,20 +41,12 @@ gulp.task('less', function () {
 
 // Watch changes to JS
 gulp.task('js', function() {
-  browserify(path.JS_ENTRY)
+  return browserify(path.JS_ENTRY)
     .transform(reactify)
     .bundle()
     .pipe(source(path.OUT_JS))
     .pipe(gulp.dest(path.DIST))
     .pipe(rename(path.OUT_JS_MIN))
-    .pipe(streamify(uglify()))
-    .pipe(gulp.dest(path.DIST));
-  
-  browserify(path.A_JS)
-    .bundle()
-    .pipe(source(path.OUT_A_JS))
-    .pipe(gulp.dest(path.DIST))
-    .pipe(rename(path.OUT_A_JS_MIN))
     .pipe(streamify(uglify()))
     .pipe(gulp.dest(path.DIST));
 });
@@ -64,6 +59,45 @@ gulp.task('watchLess', function () {
 // Watch JS
 gulp.task('watchJs', function () {
   gulp.watch(path.JS, ['js']);
+});
+
+// Tracking JS
+gulp.task('trackingJs', function () {
+  return browserify(path.A_JS)
+    .bundle()
+    .pipe(source(path.OUT_A_JS))
+    .pipe(gulp.dest(path.DIST))
+    .pipe(rename(path.OUT_A_JS_MIN))
+    .pipe(streamify(uglify()))
+    .pipe(gulp.dest(path.DIST));
+})
+
+// Clean DIST
+gulp.task('clean', function () {
+  return gulp.src(path.DIST, {read: false})
+    .pipe(clean());
+});
+
+// Clean Non-Minified Files
+gulp.task('cleanNonMinifiedJs', function () {
+  return gulp.src(Path.join(path.DIST, path.OUT_JS), {read: false})
+    .pipe(clean());
+});
+
+gulp.task('cleanNonMinifiedTrackingJs', function () {
+  return gulp.src(Path.join(path.DIST, path.OUT_A_JS), {read: false})
+    .pipe(clean());
+});
+
+gulp.task('cleanNonMinifiedCss', function () {
+  return gulp.src(Path.join(path.DIST, path.OUT_CSS), {read: false})
+    .pipe(clean());
+});
+
+gulp.task('release', function () {
+  runSequence('clean',
+             ['less', 'js', 'trackingJs'],
+             ['cleanNonMinifiedJs', 'cleanNonMinifiedTrackingJs', 'cleanNonMinifiedCss']);
 });
 
 gulp.task('default', ['less', 'js', 'watchLess', 'watchJs']);
